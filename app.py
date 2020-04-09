4a80a56143ce56165b6283bd145fe25f0e829664
Ahmed <prog.ahmed.eid@gmail.com>
2020-04-09 11:23:25 +0200

    [ADD] Add: Login System andImprove Desgin

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import os
 from flask import Flask, session,render_template,redirect, request, url_for
 from flask_session import Session
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 # Database
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -19,107 +21,113 @@ from barcode.writer import ImageWriter
 # from wtforms.validators import DataRequired
 
 
+from flask import Blueprint
+from flask_paginate import Pagination, get_page_parameter, get_page_args
+
+mod = Blueprint('student', __name__)
+
+
 UPLOAD_FOLDER = '/files/'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# configure session to use filesystem (instead of signed cookies)
+# app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 Session(app)
 
 db = sqlite3.connect('student.db', check_same_thread=False)
 
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 
    # User Register Page #
-# @app.route("/signup", methods=['GET','POST'])
-# def signup():
-#
-#     if request.method == 'POST':
-#         name = request.form.get("name")
-#         email = request.form.get("email")
-#         password = generate_password_hash(request.form.get("password"))
-#         print (password)
-#         if db.execute("SELECT email FROM users WHERE email = :email", {"email": email}).rowcount == 0:
-#             db.execute("INSERT INTO users (name, email, password) VALUES(:name, :email, :password)", {"name":name, "email": email,"password":password})
-#             db.commit()
-#
-#             # Get Username to store Session
-#             user = db.execute("SELECT id FROM users WHERE email = :email",{"email":email}).fetchone()
-#             session['user_id'] = user[0]
-#             # session['username'] = user[1]
-#             # redirect to Home
-#             return redirect("/")
-#     else:
-#         message = "Invalid Register"
-#         return render_template("signup.html", message=message)
+@app.route("/signup", methods=['GET','POST'])
+def signup():
+    # session.clear()
+    if request.method == 'POST':
+        username = request.form.get("name")
+        email = request.form.get("email")
+        password = generate_password_hash(request.form.get("password"))
+        print (password)
+        check_email = db.execute("SELECT email FROM users WHERE email = :email", {"email": email}).rowcount
+        print(check_email)
+        if check_email+1 == 0:
+            db.execute("INSERT INTO users (username, email, password) VALUES(:username, :email, :password)", {"username":username, "email": email,"password":password})
+            db.commit()
+
+            # Get Username to store Session
+            user = db.execute("SELECT id,username FROM users WHERE email = :email",{"email":email}).fetchone()
+            print(user)
+            # print(session['secret_key'])
+            session['userid'] = user[0]
+            session['username'] = user[1]
+            # redirect to Home
+            return redirect("/")
+        else:
+            return render_template("signup.html")
+    else:
+        message = "Invalid Register"
+        print(message)
+        return render_template("signup.html", message=message)
 
 
 # class MyForm(FlaskForm):
 #     name = StringField('name', validators=[DataRequired()])
 #     print(name)
 
-# @app.route('/login', methods=('GET', 'POST'))
-# def login():
-#     # form = MyForm()
-#     # if form.validate_on_submit():
-#     #     return redirect('/success')
-#     # Forget All Session_id
-#     session.clear()
-#
-#     if request.method == "POST":
-#         email = request.form.get("username")
-#         password = request.form.get("password")
-#         print(password)
-#         # hash_password = generate_password_hash(password)
-#         # print(hash_password)
-#         result = db.execute("SELECT * FROM users WHERE email = :email",{"email":email}).fetchone()
-#         print(result)
-#         print(result[3])
-#         if result:
-#             # session["user_id"] = result[0]
-#             # session["user_name"] = result[1]
-#             # return redirect("/")
-#             print(check_password_hash(result[3], password))
-#             return render_template("login.html")
-#         else:
-#             print("Error")
-#             return render_template("login.html")
-#
-#     return render_template('login.html')
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+
+    # Forget All Session_id
+    session.clear()
+    if request.method == "POST":
+        email = request.form.get("username")
+        password = request.form.get("password")
+        hash_password = generate_password_hash(password)
+        result = db.execute("SELECT * FROM users WHERE email = :email",{"email":email}).fetchone()
+
+        if result:
+            if check_password_hash(result[3], password):
+                session["user_id"] = result[0]
+                session["user_name"] = result[1]
+
+                return redirect('/')
+            else:
+                # flask.flash('Invalid email or password')
+
+                return redirect("/login")
+        else:
+            message = "Incorrect Email"
+            return render_template("login.html",message="message")
+
+    return render_template('login.html')
 
 # if not current_user.is_authenticated:
 #     return current_app.login_manager.unauthorized()
 
-# @app.route('/loginform')
-# def LoginForm():
-#     return render_template("login.html")
-
-#
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     # Here we use a class of some kind to represent and validate our
-#     # client-side form data. For example, WTForms is a library that will
-#     # handle this for us, and we use a custom LoginForm to validate.
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         # Login and validate the user.
-#         # user should be an instance of your `User` class
-#         login_user(user)
-#
-#         flask.flash('Logged in successfully.')
-#
-#         next = flask.request.args.get('next')
-#         # is_safe_url should check if the url is safe for redirects.
-#         # See http://flask.pocoo.org/snippets/62/ for an example.
-#         if not is_safe_url(next):
-#             return flask.abort(400)
-#
-#         return flask.redirect(next or flask.url_for('index'))
-#     return flask.render_template('login.html', form=form)
 
 @app.route("/logout")
 @login_required
 def logout():
-    logout_user()
-    return redirect(somewhere)
+    # logout_user()
+    session.clear()
+    return redirect('/login')
 
 
 # @mod.route('/')
@@ -146,11 +154,6 @@ def logout():
 #                            )
 
 
-from flask import Blueprint
-from flask_paginate import Pagination, get_page_parameter, get_page_args
-
-mod = Blueprint('student', __name__)
-
 
 
 def get_users(offset=0, per_page=10):
@@ -158,6 +161,7 @@ def get_users(offset=0, per_page=10):
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
 
     # page, per_page, offset = get_page_args(page_parameter='page',per_page_parameter='per_page')
@@ -267,4 +271,15 @@ def delete_all():
     return redirect(url_for("index"))
 
 if __name__ == '__main__':
+    # app.config.update(SECRET_KEY=os.urandom(24))
+    app.secret_key = 'super secret key'
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    sess.init_app(app)
+
+
+
+
+    app.debug = True
     app.run()
