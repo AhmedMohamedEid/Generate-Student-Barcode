8a6b31ac4a55eaafbd7f28b96ab28e675323c8bd
Ahmed <prog.ahmed.eid@gmail.com>
2020-05-12 01:14:30 +0200

    Last Update

--- app.py
+++ app.py
@@ -109,6 +109,26 @@ def insert_initial_values(*args, **kwargs):
 # event.listen(Users.__table__, 'after_create', insert_initial_values)
 
 
+from sqlalchemy.ext.declarative import DeclarativeMeta
+
+class AlchemyEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            # an SQLAlchemy class
+            fields = {}
+            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                data = obj.__getattribute__(field)
+                try:
+                    json.dumps(data) # this will fail on non-encodable values, like other classes
+                    fields[field] = data
+                except TypeError:
+                    fields[field] = None
+            # a json-encodable dict
+            return fields
+
+        return json.JSONEncoder.default(self, obj)
+
 
    # User Register Page #
 @app.route("/signup", methods=['GET','POST'])
